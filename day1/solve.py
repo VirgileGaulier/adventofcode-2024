a28acde4b637a16c100ea348b54cc9ad65f80f65
Virgile Gaulier <vgaulier@monsuivilogement.fr>
2024-12-03 13:28:34 +0100

    Day 1 resolve

---
+++ day1/solve.py
@@ -0,0 +1,20 @@
+list_1 = []
+list_2 = []
+
+input_file = open("input.txt", "r")
+for line in input_file:
+  line_splitted = line.split()
+  list_1.append(int(line_splitted[0]))
+  list_2.append(int(line_splitted[1]))
+
+list_1.sort()
+list_2.sort()
+
+i=0
+total_distance = 0
+for list_1_value in list_1:
+    distance = abs(list_1_value - list_2[i])
+    i += 1
+    total_distance+= distance
+
+print(total_distance)
